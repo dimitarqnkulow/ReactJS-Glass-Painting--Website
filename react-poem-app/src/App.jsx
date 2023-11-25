a56d7cf6b6fe7c15fe1b8a44ab5cba648b7ddfa3
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-11-25 16:21:11 +0200

    Add firebase to project

--- react-poem-app/src/App.jsx
+++ react-poem-app/src/App.jsx
@@ -1,30 +1,29 @@
+import { useEffect, useState } from "react";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 function App() {
-  const [count, setCount] = useState(0)
+  const [articles, setArticles] = useState([]);
+  const usersCollectionRef = collection(db, "articles");
 
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setArticles(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            {" "}
+            <h1>Article:{article.articleName}</h1>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-poem-app/src/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyBzfiq1qBergWYV--gvISfYpfoW2U-AvyI",
+
+  authDomain: "glass-painting-30b2e.firebaseapp.com",
+
+  projectId: "glass-painting-30b2e",
+
+  storageBucket: "glass-painting-30b2e.appspot.com",
+
+  messagingSenderId: "641043949351",
+
+  appId: "1:641043949351:web:d5ac50143645624242ab02",
+
+  measurementId: "G-ETXNJ9BZD7",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
