43c728cef2f524a769e015d01fed53e207ddd5c2
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-12-02 20:25:18 +0200

    Fix auth services

--- react-poem-app/src/services/authServices.js
+++ react-poem-app/src/services/authServices.js
@@ -5,17 +5,17 @@ import {
 } from "firebase/auth";
 import { auth } from "../config/firebase-config";
 
-export const register = async (email, password, repeatPassword) => {
+export const register = (email, password, repeatPassword) => {
   if (password !== repeatPassword) {
-    return new Error("Passwords doesn't match!");
+    throw new Error("Passwords doesn't match!");
   }
-  return await createUserWithEmailAndPassword(auth, email, password);
+  return createUserWithEmailAndPassword(auth, email, password);
 };
 
 export const login = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
 
-export const logout = async () => {
-  return await signOut(auth);
+export const logout = () => {
+  return signOut(auth);
 };
