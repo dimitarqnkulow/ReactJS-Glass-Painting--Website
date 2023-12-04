dd714352e38fc4a4abd5413b8188c9e8cdc6976d
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-12-04 21:40:28 +0200

    Add one article service

--- react-poem-app/src/components/Details/Details.jsx
+++ react-poem-app/src/components/Details/Details.jsx
@@ -1,55 +1,71 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import * as articleServices from "../../services/articlesServices";
+
 export default function Details() {
+  const { articleId } = useParams();
+  const [article, setArticle] = useState([]);
+  console.log(articleId);
+  useEffect(() => {
+    articleServices
+      .getOneArticle(articleId)
+      .then((result) => setArticle(result));
+  }, []);
+  console.log(article);
   return (
     <div>
-      <div class="details_page_header">
-        <div class="header">
-          <a class="back_button"> &larr; Back</a>
-          <h1 class="heading">Product Details</h1>
+      <div className="details_page_header">
+        <div className="header">
+          <a className="back_button"> &larr; Back</a>
+          <h1 className="heading">Product Details</h1>
         </div>
       </div>
 
-      <div class="product_wrapper">
-        <div class="product_box">
-          <div class="image_wrapper">
-            <img class="product_image" src="{{ article.img }}" />
+      <div className="product_wrapper">
+        <div className="product_box">
+          <div className="image_wrapper">
+            <img className="product_image_details" src={article.imgUrl} />
           </div>
 
-          <div class="name_and_description_wrapper">
-            <h2 class="product_name">{}</h2>
+          <div className="name_and_description_wrapper">
+            <h2 className="product_name">{article.articleName}</h2>
 
-            <div class="likes_counter">
+            <div className="likes_counter">
               <img id="likes_icon" src="assets/likes_count.png" />
 
-              <p class="likes_count">{}</p>
+              <p className="likes_count">{}</p>
             </div>
-            <p class="product_material">Optional price</p>
-            <div class="product_card_price">
-              ${}
-              <span class="product_card_discount_price">$</span>
-            </div>
-
-            <div class="product_description_wrapper">
-              <p class="product_material">Description</p>
-
-              <p class="product_description">{}</p>
+            <p className="product_material">Optional price</p>
+            <div className="product_card_price">
+              ${article.price}
+              <span className="product_card_discount_price">
+                ${article.price * 1.25}
+              </span>
             </div>
 
-            <div class="product_description_wrapper">
-              <p class="product_material">Materials</p>
+            <div className="product_description_wrapper">
+              <p className="product_material">Description</p>
 
-              <p class="product_description">{}</p>
+              <p className="product_description">{article.description}</p>
             </div>
 
-            <div class="product_description_wrapper">
-              <p class="product_material">Crafting time</p>
+            <div className="product_description_wrapper">
+              <p className="product_material">Materials</p>
 
-              <p class="product_description">{} days</p>
+              <p className="product_description">{article.materials}</p>
             </div>
 
-            <div class="buttons_wrapper">
-              <div class="like_icon"></div>
+            <div className="product_description_wrapper">
+              <p className="product_material">Crafting time</p>
 
-              <div class="unlike_icon"></div>
+              <p className="product_description">{article.crafting} days</p>
+            </div>
+
+            <div className="buttons_wrapper">
+              <div className="like_icon"></div>
+
+              <div className="unlike_icon"></div>
             </div>
           </div>
         </div>
--- react-poem-app/src/services/articlesServices.js
+++ react-poem-app/src/services/articlesServices.js
@@ -1,5 +1,5 @@
 import { db } from "../config/firebase-config";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 const articlesCollectionRef = collection(db, "articles");
 
@@ -7,4 +7,11 @@ export const getAllArticles = async () => {
   const data = await getDocs(articlesCollectionRef);
 
   return data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+};
+
+export const getOneArticle = async (id) => {
+  const articleRef = doc(db, "articles", id);
+  const data = await getDoc(articleRef);
+
+  return data.data();
 };
