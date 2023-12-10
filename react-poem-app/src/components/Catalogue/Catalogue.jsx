c5adc2e14aff6aac14c9b4d20aca97ccb88936bd
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-12-10 09:12:22 +0200

    Change h1 on catalogue page

--- react-poem-app/src/components/Catalogue/Catalogue.jsx
+++ react-poem-app/src/components/Catalogue/Catalogue.jsx
@@ -12,13 +12,13 @@ export default function Catalogue() {
   return (
     <div className="catalogue_box">
       <div className="search_wrapper">
-        <h1 className="catalogue_header">Ecommerce Acceories & Fashion item</h1>
-        <div className="search_bar_wrapper">
+        <h1 className="catalogue_header">Catalogue of Available items</h1>
+        {/* <div className="search_bar_wrapper">
           <input className="search_bar_input" type="text" />
           <button className="search_bar_button" type="submit">
             <img src="assets/uil_search.svg" />
           </button>
-        </div>
+        </div> */}
       </div>
 
       <div className="articles_wrapper">
